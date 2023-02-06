5913d720a2ec45137ad6d1fdba7b34c4d86e64e5
Nara-888 <103151337+Nara-888@users.noreply.github.com>
2023-02-07 00:10:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace _02._Summer_Outfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int grade = int.Parse(Console.ReadLine());
+            string partOfDay = Console.ReadLine();
+
+            string outfit = "";
+            string shoes = "";
+
+            switch (partOfDay)
+            {
+
+                case "Morning":
+                    if (grade >= 10 &&  grade <= 18)
+                    {
+                        outfit = "Sweatshirt";
+                        shoes = "Sneakers";
+                    }
+                    else if (grade > 18 && grade <= 24 )
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else
+                    {
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                    }
+                    break;
+                case "Afternoon":
+                    if (grade >= 10 && grade <= 18)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else if (grade > 18 && grade <= 24)
+                    {
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                    }
+                    else
+                    {
+                        outfit = "Swim Suit";
+                        shoes = "Barefoot";
+                    }
+                    break;
+                case "Evening":
+                    if (grade >= 10 && grade <= 18)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else if (grade > 18 && grade <= 24)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    break;
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+            Console.WriteLine($"It's {grade} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
---
+++ hotelRoom.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _07._Hotel_Room
+{
+    internal class hotelRoom
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int numberOfNights = int.Parse(Console.ReadLine());
+
+            double priceStudio = 0;
+            double priceApartment = 0;
+
+            switch (month)
+            {
+                case "May":
+                case "October":
+
+                    if (numberOfNights > 7 && numberOfNights <= 14)
+                    {
+                        priceApartment = numberOfNights * 65;
+                        priceStudio = (numberOfNights * 50) * 0.95;
+                    }
+                    else if (numberOfNights > 14)
+                    {
+                        priceApartment = (numberOfNights * 65) * 0.90;
+                        priceStudio = (numberOfNights * 50) * 0.70;
+                    }
+                    else
+                    {
+                        priceApartment = numberOfNights * 65;
+                        priceStudio = numberOfNights * 50;
+                    }
+                    break;
+
+                case "June":
+                case "September":
+                    if (numberOfNights > 14)
+                    {
+                        priceApartment = (numberOfNights * 68.70) * 0.90;
+                        priceStudio = (numberOfNights * 75.20) * 0.80;
+                    }
+                    else
+                    {
+                        priceApartment = numberOfNights * 68.70;
+                        priceStudio = numberOfNights * 75.20;
+                    }
+                    break;
+                case "July":
+                case "August":
+                    if (numberOfNights > 14)
+                    {
+                        priceApartment = (numberOfNights * 77) * 0.90;
+                        
+                    }
+                    else
+                    {
+                        priceApartment = numberOfNights * 77;
+                        
+                        
+                    }
+                    priceStudio = numberOfNights * 76;
+                    break;
+            }
+
+
+            Console.WriteLine($"Apartment: {priceApartment:F2} lv.");
+            Console.WriteLine($"Studio: {priceStudio:F2} lv.");
+
+
+            //OUTPUT 	•	На първия ред: “Apartment: {цена за целият престой} lv.
+            //”На втория ред: “Studio: { цена за целият престой}
+            //lv.“
+
+
+        }
+    }
+}
