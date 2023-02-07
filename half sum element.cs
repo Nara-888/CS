0f7c6e6a3724d82bf6085b12edb822d7c873003b
Nara-888 <103151337+Nara-888@users.noreply.github.com>
2023-02-07 21:00:59 +0200

    Add files via upload

---
+++ half sum element.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02._Half_Sum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int maxNum = int.MinValue;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+                if (currentNum > maxNum)
+                {
+                    maxNum = currentNum;
+                }
+                
+            }
+            int maxNumberSubtractedFromSum = sum - maxNum;
+
+            if (maxNumberSubtractedFromSum == maxNum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = "+maxNumberSubtractedFromSum);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + Math.Abs(maxNumberSubtractedFromSum -maxNum));
+            }
+        }
+    }
+}
