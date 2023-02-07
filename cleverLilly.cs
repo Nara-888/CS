c8750c0fdcf4390e42ccae23b3c0276ef0346fb7
Nara-888 <103151337+Nara-888@users.noreply.github.com>
2023-02-07 21:01:56 +0200

    Add files via upload

---
+++ cleverLilly.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _04._Clever_Lily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double priceWashingMachine = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+
+            int toysCounter = 0;
+            int savedMoney = 0;
+            int moneyTakenByLillysBrother = 0;
+            int moneyAdded = 10;
+
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    toysCounter++;
+                }
+                else
+                {
+                    
+                    savedMoney += moneyAdded;
+                    moneyAdded += 10;
+                    moneyTakenByLillysBrother++;
+                }
+            }
+
+            int finalAmountSaved = savedMoney + toysCounter * toyPrice - moneyTakenByLillysBrother;
+
+            if (finalAmountSaved >= priceWashingMachine)
+            {
+                Console.WriteLine($"Yes! {(double)finalAmountSaved -priceWashingMachine:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {priceWashingMachine - (double)finalAmountSaved:F2}");
+            }
+        }
+    }
+}
